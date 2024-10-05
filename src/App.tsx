0fb6a6783af40f2cd50a9a6079d155b0036bb3c5
Nattapong Promthong <banker.z412@gmail.com>
2024-10-05 22:27:15 +0700

    feat: code starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [storedValue, setStoredValue] = useState("No value");
+
+  useEffect(() => {
+    const value = localStorage.getItem("myValue");
+    if (value) {
+      setStoredValue(value);
+    }
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="flex flex-col gap-2 justify-center items-center w-full min-h-screen">
+        <h1 className="text-6xl font-bold text-center">
+          React Native Webview POC
+        </h1>
+        <div className="text-2xl text-center">
+          Try passing value from mobile to Web application
+        </div>
+        <p className="mt-4 text-2xl text-center">
+          Stored Value From Mobile: {storedValue}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
