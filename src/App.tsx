ea2a9463439b4ef05778cbeab2134884019e5287
Nattapong Promthong <banker.z412@gmail.com>
2024-10-08 10:56:59 +0700

    feat: fix warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const WebApp = () => {
   const [messageFromReactNative, setMessageFromReactNative] = useState("");
